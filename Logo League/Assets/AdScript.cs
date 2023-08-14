42157f9dbcccc59c90913517ac505113227d3af1
Shay Maryuma <56411971+shayke223@users.noreply.github.com>
2023-08-14 12:59:23 +0300

    Update AdScript.cs

--- Logo League/Assets/AdScript.cs
+++ Logo League/Assets/AdScript.cs
@@ -22,22 +22,7 @@ public class AdScript : MonoBehaviour
 
     private void showBannerAd()
     {
-     string adID = "ca-app-pub-3940256099942544/6300978111";
-      // string adID = "ca-app-pub-2519339969103025/6249431668";
-     
-       
-        //***For Testing in the Device***
-        AdRequest request = new AdRequest.Builder()
-       .AddTestDevice(AdRequest.TestDeviceSimulator)       // Simulator.
-       .AddTestDevice("2077ef9a63d2b398840261c8221a0c9b")  // My test device.
-       .Build();
-   
-
-        //***For Production When Submit App***
-     //  AdRequest request = new AdRequest.Builder().Build();
-
-        BannerView bannerAd = new BannerView(adID, AdSize.SmartBanner, AdPosition.Bottom);
-        bannerAd.LoadAd(request);
+         //  deleted!
     }
 
 
